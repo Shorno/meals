df0eb28bb81c375a0e6fef091d6c94a9859da8ce
faiyaz <ahmed15-6413@s.diu.edu.bd>
2026-04-09 22:59:34 +0600

    test push

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <ShareSection />
         <DiscoverSection />
           <div>
-              hello
+              hello...
           </div>
       </main>
       <Footer />
